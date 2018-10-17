0fba92c0a0d58bd19744344723f6fdf5b67879ed
Mikael Ragnhult <mikaelragnhult@spotify.com>
2018-10-17 22:52:29 +0200

    Empty app

---
+++ app/src/main/java/se/tussa/clean/presentation/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package se.tussa.clean.presentation.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import se.tussa.clean.R;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
